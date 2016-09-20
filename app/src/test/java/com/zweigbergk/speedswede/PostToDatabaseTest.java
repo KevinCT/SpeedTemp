f6d0a4dabe22e94782bef530d451a3a03019796d
ryggan <andreas@carlssonfoto.se>
2016-09-20 11:39:53 +0200

    Adding test stub

---
+++ app/src/test/java/com/zweigbergk/speedswede/PostToDatabaseTest.java
@@ -0,0 +1,14 @@
+package com.zweigbergk.speedswede;
+
+import org.junit.Test;
+import java.util.regex.Pattern;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class PostToDatabaseTest {
+
+    public void simpleTest() {
+        assertTrue(1 == 1);
+    }
+
+}
