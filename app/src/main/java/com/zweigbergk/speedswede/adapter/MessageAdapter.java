78f6316c07270188747d5dc02ef2ed822e2e2b1c
Kevin Chen Trieu <kevinchentrieu@gmail.com>
2016-09-22 00:31:08 +0200

    Integrated GUI with messageadapter

--- app/src/main/java/com/zweigbergk/speedswede/adapter/MessageAdapter.java
+++ app/src/main/java/com/zweigbergk/speedswede/adapter/MessageAdapter.java
@@ -1,58 +1,65 @@
 package com.zweigbergk.speedswede.adapter;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.TextView;
 
+import com.zweigbergk.speedswede.R;
 import com.zweigbergk.speedswede.core.Message;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.ViewHolder> {
-    private List<Message> messageList;
+public class MessageAdapter extends BaseAdapter {
+    private List<Message> mMessageList;
+    private View mView;
+    private TextView mName;
+    private TextView mText;
+    private TextView mTimeStamp;
 
     public MessageAdapter(){
-        messageList = new ArrayList<>();
-    }
-    @Override
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        //waiting for layouts.
-        //View messageView = LayoutInflater.from(parent.getContext()).inflate(R.,parent,false);
-        //ViewHolder viewHolder = new ViewHolder(messageView);
-        return null;
+        mMessageList=new ArrayList<>();
+        Message message1 = new Message("apa", "Jag lära svenska!", 123);
+        Message message2 = new Message("apa", "S", 123);
+        Message message3 = new Message("apa", "hallå där", 123);
+        Message message4 = new Message("apa", "hallå där", 123);
+
+        mMessageList.add(message1);
+        mMessageList.add(message2);
+        mMessageList.add(message3);
+        mMessageList.add(message4);
 
     }
 
-    @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
-        Message message = messageList.get(position);
-        holder.nameView.setText(message.getName());
-        holder.textView.setText(message.getText());
-        holder.timeStampView.setText("" + message.getTimeStamp());
-
-    }
 
     @Override
-    public int getItemCount() {
-        return messageList.size();
+    public int getCount() {
+        return mMessageList.size();
     }
 
+    @Override
+    public Object getItem(int i) {
+        return mMessageList.indexOf(i);
+    }
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
-        private TextView nameView;
-        private TextView textView;
-        private TextView timeStampView;
-        public ViewHolder(View itemView) {
-            super(itemView);
-            /*message layout not available yet
-            nameView =(TextView) itemView.findViewById(R.id.cast_notification_id);
-            textView =(TextView) itemView.findViewById(R.id.cast_notification_id);
-            timeStampView =(TextView) itemView.findViewById(R.id.cast_notification_id);
-            */
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
 
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        //all messages are seen as user now, unique variable needed to separate between user messages
+        if(view==null){
 
+            mView = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.fragment_message_user,viewGroup,false);
         }
+
+        mText = (TextView) mView.findViewById(R.id.message_textview_user);
+        mText.setText(mMessageList.get(i).getText());
+        return mView;
     }
 }
--- app/src/main/java/com/zweigbergk/speedswede/fragment/ChatFragment.java
+++ app/src/main/java/com/zweigbergk/speedswede/fragment/ChatFragment.java
@@ -6,13 +6,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
 
 import com.zweigbergk.speedswede.R;
+import com.zweigbergk.speedswede.adapter.MessageAdapter;
 
 public class ChatFragment extends Fragment {
+    private MessageAdapter mMessageAdapter;
+    private ListView chatView;
 
     public ChatFragment() {
         // Required empty public constructor
@@ -31,8 +35,12 @@ public class ChatFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_chat, container, false);
         View message = inflater.inflate(R.layout.fragment_message_user, null);
-        ((TextView)message.findViewById(R.id.message_textview_user)).setText("Hello");
-        ((LinearLayout)view.findViewById(R.id.fragment_chat_linearlayout)).addView(message);
+        //((TextView)message.findViewById(R.id.message_textview_user)).setText("Hello");
+        //((LinearLayout)view.findViewById(R.id.fragment_chat_linearlayout)).addView(message);
+        chatView = (ListView) view.findViewById(R.id.fragment_message_view);
+        mMessageAdapter = new MessageAdapter();
+        chatView.setAdapter(mMessageAdapter);
+
         return view;
     }
 }
