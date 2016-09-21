4cf82fd99e0f8bf22ecac944c4484541104c333b
Felix Engelbrektsson <ugglan00@gmail.com>
2016-09-21 16:50:17 +0200

    bla bla

---
+++ app/src/main/java/com/zweigbergk/speedswede/fragment/MessageFragmentUser.java
@@ -0,0 +1,26 @@
+package com.zweigbergk.speedswede.fragment;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.zweigbergk.speedswede.ChatActivity;
+import com.zweigbergk.speedswede.R;
+
+/**
+ * Created by FEngelbrektsson on 21/09/16.
+ */
+
+public class MessageFragmentUser {
+
+    private String DUMMY_MESSAGE = "Tjena";
+    public void sendMessage(View view) {
+        //Intent intent = new Intent(this, ChatActivity.class);
+        //EditText editText = (EditText) findViewById(R.id.edit_message);
+        //String message = editText.getText().toString();
+        //intent.putExtra(DUMMY_MESSAGE, message);
+        //startActivity(intent);
+    }
+}
