bdf0893454ffcde001d86ad9fc3abd3d89a95bde
ryggan <andreas@carlssonfoto.se>
2016-09-27 11:14:19 +0200

    Implementing methods in Chat class

--- app/src/main/java/com/zweigbergk/speedswede/core/Chat.java
+++ app/src/main/java/com/zweigbergk/speedswede/core/Chat.java
@@ -1,39 +1,47 @@
 package com.zweigbergk.speedswede.core;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class Chat {
 
     private final User mFirstUser, mSecondUser;
     private List<Message> mConversation;
-    private String mUid;
+    private String mId;
+    private long mTimeStamp;
 
     public Chat(User firstUser, User secondUser) {
         mFirstUser = firstUser;
         mSecondUser = secondUser;
 
         mConversation = new ArrayList<>();
-        mUid = Long.toString(firstUser.getUid().hashCode() * 17 + secondUser.getUid().hashCode() * 31);
+        mId = Long.toString(firstUser.getUid().hashCode() * 17 + secondUser.getUid().hashCode() * 31);
+
+        mTimeStamp = (new Date()).getTime();
     }
     public boolean includesUser(User user) {
-        return false;
+        return mFirstUser.equals(user) || mSecondUser.equals(user);
     }
 
     public User getFirstUser() {
-        return null;
+        return mFirstUser;
     }
 
     public User getSecondUser() {
-        return null;
+        return mSecondUser;
+    }
+
+    public long getTimeStamp() {
+        return mTimeStamp;
     }
 
     public List<Message> getConversation() {
         return null;
     }
     
-    public String getUid() {
-        return mUid;
+    public String getId() {
+        return mId;
     }
 
     public void postMessage(User user, Message message) throws IllegalArgumentException {
