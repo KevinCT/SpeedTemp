44968a035128d64cc0b037e3edddea54d888d3b1
Jonathan Kran <jonathan.kran1@gmail.com>
2016-09-27 18:03:07 +0200

    Update view interfaces

--- app/src/main/java/com/zweigbergk/speedswede/view/LoginView.java
+++ app/src/main/java/com/zweigbergk/speedswede/view/LoginView.java
@@ -1,13 +1,11 @@
 package com.zweigbergk.speedswede.view;
 
 import android.view.View;
-import android.widget.Button;
 
 import com.facebook.login.widget.LoginButton;
 
 public interface LoginView {
     LoginButton getLoginButton();
-//    Button getLoginButton();
     void startChatActivity();
     void onLoginClick(View.OnClickListener listener);
     void showProgressCircle();
