3d9b51c81fd4fc846dac02ed64bd6d5a058de5d8
ryggan <andreas@carlssonfoto.se>
2016-09-28 11:30:28 +0200

    Working on connecting two chats

--- app/src/main/java/com/zweigbergk/speedswede/fragment/ChatFragment.java
+++ app/src/main/java/com/zweigbergk/speedswede/fragment/ChatFragment.java
@@ -39,7 +39,7 @@ public class ChatFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_chat, container, false);
 
-        if (mCurrentChatId == null || mCurrentChatId = "") {
+        if (mCurrentChatId == null || mCurrentChatId.equals("")) {
             mCurrentChatId = "privateChatUser_"+DatabaseHandler.INSTANCE.getLoggedInUser().getUid();
         }
 //        mCurrentChatId = "privateChatUser_742nxCA9qvUF4ZIRGqA9sWfgooH2"; // Andreas facebook-id
