2de3bf96e0f7895e96469e939273516a8a3425db
Jonathan Kran <jonathan.kran1@gmail.com>
2016-10-03 16:35:55 +0200

    Resolve merge issue with R file

--- app/src/main/java/com/zweigbergk/speedswede/fragment/ChatListFragment.java
+++ app/src/main/java/com/zweigbergk/speedswede/fragment/ChatListFragment.java
@@ -45,9 +45,6 @@ public class ChatListFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
-
-
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_chat_list, container, false);
 
