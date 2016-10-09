acce3ab580d9f18c7cb8686ed4c2982ca362f7a9
Jonathan Kran <jonathan.kran1@gmail.com>
2016-10-09 18:41:43 +0200

    Place ChatFragment logic in its presenter

--- app/src/main/java/com/zweigbergk/speedswede/fragment/ChatFragment.java
+++ app/src/main/java/com/zweigbergk/speedswede/fragment/ChatFragment.java
@@ -1,12 +1,9 @@
 package com.zweigbergk.speedswede.fragment;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -16,36 +13,57 @@ import android.view.ViewGroup;
 import android.widget.EditText;
 
 import com.zweigbergk.speedswede.R;
-import com.zweigbergk.speedswede.adapter.MessageAdapter;
+import com.zweigbergk.speedswede.activity.ChatActivity;
 import com.zweigbergk.speedswede.core.Chat;
-import com.zweigbergk.speedswede.core.Message;
-import com.zweigbergk.speedswede.core.User;
-import com.zweigbergk.speedswede.database.DataChange;
-import com.zweigbergk.speedswede.database.DatabaseEvent;
-import com.zweigbergk.speedswede.database.DatabaseHandler;
-import com.zweigbergk.speedswede.interactor.BanInteractor;
 import com.zweigbergk.speedswede.presenter.ChatFragmentPresenter;
-import com.zweigbergk.speedswede.util.Client;
-import com.zweigbergk.speedswede.util.Lists;
+import com.zweigbergk.speedswede.util.CallerMethod;
+import com.zweigbergk.speedswede.util.ProviderMethod;
+import com.zweigbergk.speedswede.view.ChatFragmentView;
 
-import java.util.Collections;
-import java.util.Date;
-
-public class ChatFragment extends Fragment implements Client<DataChange<Message>> {
+public class ChatFragment extends Fragment implements ChatFragmentView {
     public static final String TAG = ChatFragment.class.getSimpleName().toUpperCase();
 
     private RecyclerView chatRecyclerView;
-    private Chat mChat;
+    private EditText mInputBox;
+
     //TODO presenter between interactor and fragment
     private ChatFragmentPresenter mPresenter;
 
-    private EditText mInputBox;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setHasOptionsMenu(true);
         mPresenter = new ChatFragmentPresenter();
-        chatListAdapter = new ChatListAdapter();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_chat, container, false);
+
+        view.findViewById(R.id.fragment_chat_post_message).setOnClickListener(this::onButtonClick);
+
+        chatRecyclerView = (RecyclerView) view.findViewById(R.id.fragment_chat_recycler_view);
+        mInputBox = (EditText) view.findViewById(R.id.fragment_chat_message_text);
+
+        return view;
+    }
+
+    public void setChat(Chat newChat) {
+        mPresenter.onChatChanged(newChat);
+    }
+
+    private void onButtonClick(View view) {
+        mPresenter.onClickSend();
+    }
+
+    @Override
+    public String getInputText() {
+        return mInputBox.getText().toString();
+    }
+
+    @Override
+    public void clearInputField() {
+        mInputBox.setText("");
     }
 
     @Override
@@ -58,124 +76,35 @@ public class ChatFragment extends Fragment implements Client<DataChange<Message>
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.blockUser:
-                mPresenter.onBanClicked(mChat.getFirstUser().getUid(),mChat.getSecondUser().getUid());
+                mPresenter.onBanClicked();
                 return true;
             case R.id.changeLangauge:
-                FragmentManager manager = getFragmentManager();
-                FragmentTransaction transaction = manager.beginTransaction();
-                transaction.add(android.R.id.content, new ChangeLanguageFragment());
-                transaction.addToBackStack(null);
-                transaction.commit();
+                mPresenter.onChangeLanguageClicked();
                 return true;
             case R.id.exitChat:
-                terminateChat();
+                mPresenter.terminateChat();
             default:
                 return super.onOptionsItemSelected(item);
         }
     }
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_chat, container, false);
-
-        view.findViewById(R.id.fragment_chat_post_message).setOnClickListener(this::onButtonClick);
-
-        initializeRecyclerView(view);
-
-        mInputBox = (EditText) view.findViewById(R.id.fragment_chat_message_text);
-
-        return view;
+    public RecyclerView getRecyclerView() {
+        return chatRecyclerView;
     }
 
-    public void terminateChat() {
-        User activeUser = DatabaseHandler.getActiveUser();
-
-        DatabaseHandler.get(mChat).removeUser(activeUser);
-    }
-
-    public void setChat(Chat newChat) {
-        Chat oldChat = mChat;
-
-        if (oldChat != null && oldChat.equals(newChat)) {
-            return;
-        }
-
-        getMessageAdapter().clear();
-
-        //We no longer want updates from the old chat. Remove us as a client from the old chat.
-        if (oldChat != null) {
-            DatabaseHandler.get(oldChat).unbindMessageClient(this);
-        }
-
-        //We DO want updates from the new chat! Add us as a client to that one :)
-        DatabaseHandler.get(newChat).bindMessageClient(this);
-
-        mChat = newChat;
-    }
-
-    private void onButtonClick(View view) {
-        String messageText = mInputBox.getText().toString();
-
-        if (messageText.trim().length() > 0) {
-            postMessage(messageText);
-        }
-    }
-
-    private void postMessage(String text) {
-        Message message = new Message(DatabaseHandler.getActiveUserId(), text, getCurrentTime());
-        DatabaseHandler.get(mChat).sendMessage(message);
-
-        MessageAdapter adapter = getMessageAdapter();
-        adapter.onListChanged(DataChange.added(message));
-
-        clearInputField();
-    }
-
-    private MessageAdapter getMessageAdapter() {
-        return (MessageAdapter) chatRecyclerView.getAdapter();
-    }
-
-    private void clearInputField() {
-        mInputBox.setText("");
-    }
-
-    private long getCurrentTime() {
-        return new Date().getTime();
-    }
-
-    private void initializeRecyclerView(View view) {
-        chatRecyclerView = (RecyclerView) view.findViewById(R.id.fragment_chat_recycler_view);
-        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
+    @Override
+    public void setLayoutManager(RecyclerView.LayoutManager layoutManager) {
         chatRecyclerView.setLayoutManager(layoutManager);
-        layoutManager.setStackFromEnd(true);
-
-        MessageAdapter adapter = new MessageAdapter();
-        chatRecyclerView.setAdapter(adapter);
-
-        adapter.addEventCallback(DatabaseEvent.ADDED, this::smoothScrollToBottomOfList);
-    }
-
-    private void smoothScrollToBottomOfList(Message message) {
-        int scrollOffset = chatRecyclerView.computeVerticalScrollOffset();
-        int scrollHeight = chatRecyclerView.computeVerticalScrollRange() - chatRecyclerView.computeVerticalScrollExtent();
-
-        if (chatRecyclerView.getAdapter().getItemCount() <= 0) {
-            return;
-        }
-
-        // Only scroll to the bottom if the new message was posted by us,
-        //   OR if you are at the relative bottom of the chat.
-        if ((message.getId() != null && message.getId().equals(DatabaseHandler.getActiveUserId()))
-                || (scrollHeight - scrollOffset < chatRecyclerView.getHeight())) {
-            chatRecyclerView.smoothScrollToPosition(chatRecyclerView.getAdapter().getItemCount() - 1);
-        }
-
     }
 
     @Override
-    public void supply(DataChange<Message> dataChange) {
-        Log.d(TAG, String.format("Calling onListChanged with change: [%s] and messageText: [%s]",
-                dataChange.getEvent().toString(), dataChange.getItem().getText()));
+    public <T> T contextualize(ProviderMethod<T, Context> method) {
+        return method.call(getContext());
+    }
 
-        ((MessageAdapter)chatRecyclerView.getAdapter()).onListChanged(dataChange);
+    @Override
+    public void useActivity(CallerMethod<ChatActivity> method) {
+        method.call((ChatActivity) getActivity());
     }
 }
--- app/src/main/java/com/zweigbergk/speedswede/presenter/ChatFragmentPresenter.java
+++ app/src/main/java/com/zweigbergk/speedswede/presenter/ChatFragmentPresenter.java
@@ -1,19 +1,135 @@
 package com.zweigbergk.speedswede.presenter;
 
-import com.zweigbergk.speedswede.interactor.BanInteractor;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 
-/**
- * Created by Kevin on 2016-10-09.
- */
+import com.zweigbergk.speedswede.activity.ChatActivity;
+import com.zweigbergk.speedswede.adapter.MessageAdapter;
+import com.zweigbergk.speedswede.core.Chat;
+import com.zweigbergk.speedswede.core.Message;
+import com.zweigbergk.speedswede.core.User;
+import com.zweigbergk.speedswede.database.DataChange;
+import com.zweigbergk.speedswede.database.DatabaseEvent;
+import com.zweigbergk.speedswede.database.DatabaseHandler;
+import com.zweigbergk.speedswede.interactor.BanInteractor;
+import com.zweigbergk.speedswede.util.Client;
+import com.zweigbergk.speedswede.util.Time;
+import com.zweigbergk.speedswede.view.ChatFragmentView;
 
 public class ChatFragmentPresenter {
+    public static final String TAG = ChatFragmentPresenter.class.getSimpleName().toUpperCase();
+
     private BanInteractor mBanInteractor;
+
+    private ChatFragmentView mView;
+
+    private Chat mChat;
 
     public ChatFragmentPresenter(){
         mBanInteractor = new BanInteractor();
+
+        initializeRecyclerView();
     }
 
-    public void onBanClicked(String firstUser, String secondUser){
-        mBanInteractor.addBan(firstUser, secondUser);
+    private void initializeRecyclerView() {
+        RecyclerView recyclerView = mView.getRecyclerView();
+
+        LinearLayoutManager layoutManager = mView.contextualize(LinearLayoutManager::new);
+        recyclerView.setLayoutManager(layoutManager);
+        layoutManager.setStackFromEnd(true);
+
+        MessageAdapter adapter = new MessageAdapter();
+        recyclerView.setAdapter(adapter);
+
+        adapter.addEventCallback(DatabaseEvent.ADDED, this::smoothScrollToBottomOfList);
     }
+
+    private void smoothScrollToBottomOfList(Message message) {
+        RecyclerView recyclerView = mView.getRecyclerView();
+        int scrollOffset = recyclerView.computeVerticalScrollOffset();
+        int scrollHeight = recyclerView.computeVerticalScrollRange() - recyclerView.computeVerticalScrollExtent();
+
+        if (recyclerView.getAdapter().getItemCount() <= 0) {
+            return;
+        }
+
+        // Only scroll to the bottom if the new message was posted by us,
+        //   OR if you are at the relative bottom of the chat.
+        if ((message.getId() != null && message.getId().equals(DatabaseHandler.getActiveUserId()))
+                || (scrollHeight - scrollOffset < recyclerView.getHeight())) {
+            recyclerView.smoothScrollToPosition(recyclerView.getAdapter().getItemCount() - 1);
+        }
+    }
+
+    public void onChatChanged(Chat newChat) {
+        Chat oldChat = mChat;
+
+        if (oldChat != null && oldChat.equals(newChat)) {
+            return;
+        }
+
+        getMessageAdapter().clear();
+
+        //We no longer want updates from the old chat. Remove us as a client from the old chat.
+        if (oldChat != null) {
+            DatabaseHandler.get(oldChat).unbindMessageClient(handleChatEvent);
+        }
+
+        //We DO want updates from the new chat! Add us as a client to that one :)
+        DatabaseHandler.get(newChat).bindMessageClient(handleChatEvent);
+
+        mChat = newChat;
+    }
+
+    public void onClickSend() {
+        String messageText = mView.getInputText();
+
+        if (messageText.trim().length() > 0) {
+            postMessage(messageText);
+        }
+    }
+
+    public void terminateChat() {
+        User activeUser = DatabaseHandler.getActiveUser();
+        DatabaseHandler.get(mChat).removeUser(activeUser);
+    }
+
+    private void postMessage(String messageText) {
+        Message message = new Message(
+                DatabaseHandler.getActiveUserId(),
+                messageText,
+                Time.getCurrentTime());
+
+        DatabaseHandler.get(mChat).sendMessage(message);
+
+        addToAdapter(message);
+        mView.clearInputField();
+    }
+
+    private void addToAdapter(Message message) {
+        MessageAdapter adapter = getMessageAdapter();
+        adapter.onListChanged(DataChange.added(message));
+    }
+
+    public void onBanClicked(){
+        String firstUserId = mChat.getFirstUser().getUid();
+        String secondUserId = mChat.getSecondUser().getUid();
+        mBanInteractor.addBan(firstUserId, secondUserId);
+    }
+
+    public void onChangeLanguageClicked() {
+        mView.useActivity(ChatActivity::showLanguageFragment);
+    }
+
+    private MessageAdapter getMessageAdapter() {
+        return (MessageAdapter) mView.getRecyclerView().getAdapter();
+    }
+
+    private final Client<DataChange<Message>> handleChatEvent = dataChange -> {
+        Log.d(TAG, String.format("Calling onListChanged with change: [%s] and messageText: [%s]",
+                dataChange.getEvent().toString(), dataChange.getItem().getText()));
+
+        ((MessageAdapter) mView.getRecyclerView().getAdapter()).onListChanged(dataChange);
+    };
 }
