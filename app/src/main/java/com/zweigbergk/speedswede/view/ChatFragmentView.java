078fc102eb95b8c948142464f25c8998cea17222
Jonathan Kran <jonathan.kran1@gmail.com>
2016-10-09 18:40:57 +0200

    Create View interface for ChatFragment

---
+++ app/src/main/java/com/zweigbergk/speedswede/view/ChatFragmentView.java
@@ -0,0 +1,17 @@
+package com.zweigbergk.speedswede.view;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+
+import com.zweigbergk.speedswede.activity.ChatActivity;
+import com.zweigbergk.speedswede.util.CallerMethod;
+import com.zweigbergk.speedswede.util.ProviderMethod;
+
+public interface ChatFragmentView {
+    RecyclerView getRecyclerView();
+    void setLayoutManager(RecyclerView.LayoutManager layoutManager);
+    <T> T contextualize(ProviderMethod<T, Context> method);
+    void useActivity(CallerMethod<ChatActivity> method);
+    void clearInputField();
+    String getInputText();
+}
