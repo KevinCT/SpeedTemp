dad525bfb23380d43f689fa869225e5196cb1e92
Kevin Chen Trieu <kevinchentrieu@gmail.com>
2016-10-11 14:53:03 +0200

    updated DialogGUI, removed obsolete code from changelanguagefragment

--- app/src/main/java/com/zweigbergk/speedswede/database/LocalStorage.java
+++ app/src/main/java/com/zweigbergk/speedswede/database/LocalStorage.java
@@ -6,20 +6,30 @@ import android.preference.PreferenceManager;
 import android.util.Log;
 
 import com.zweigbergk.speedswede.Constants;
+import com.zweigbergk.speedswede.core.Pair;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
 public enum LocalStorage {
     INSTANCE;
 
     public static final String TAG = LocalStorage.class.getSimpleName().toUpperCase();
+    private static List<String>  keyList= new ArrayList();
+    private static List<String>  valueList= new ArrayList();
 
     public void saveSettings(Context context, String key, String value){
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
         SharedPreferences.Editor editor = preferences.edit();
-        editor.putString(key, value).apply();
-
-
+        keyList.add(key);
+        valueList.add(value);
+        if(keyList!=null && valueList.size()==keyList.size()){
+            for(int i=0;i<keyList.size();i++){
+                editor.putString(keyList.get(i),valueList.get(i));
+            }
+        }
+        editor.apply();
     }
 
     public String getLanguage(Context context){
