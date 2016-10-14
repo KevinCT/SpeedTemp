b2b6ea957d57d358ae693d283ce3622e8cbfe7a5
Jonathan Kran <jonathan.kran1@gmail.com>
2016-10-14 05:36:08 +0200

    Improve Statements

--- app/src/main/java/com/zweigbergk/speedswede/util/async/Statement.java
+++ app/src/main/java/com/zweigbergk/speedswede/util/async/Statement.java
@@ -1,6 +1,10 @@
 package com.zweigbergk.speedswede.util.async;
 
+import com.zweigbergk.speedswede.util.Tuple;
 import com.zweigbergk.speedswede.util.methodwrapper.Executable;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Statement extends Promise<Boolean> {
     private boolean inverted;
@@ -28,6 +32,36 @@ public class Statement extends Promise<Boolean> {
     @Override
     public void addItem(PromiseNeed need, Object data) throws StatementException {
         throw new StatementException("addItem() may not be called from within a Statement. Use a Promise instead.");
+    }
+
+    private StatementGroup slamTogether(Result<Boolean> resultForm, List<Tuple<PromiseNeed, Statement>> statements) {
+        StatementGroup group = new StatementGroup(statements);
+        group.setResultForm(resultForm);
+        return group;
+    }
+
+    public Statement and(Statement statement) {
+        List<Tuple<PromiseNeed, Statement>> combined = new ArrayList<>();
+        combined.add(new Tuple<>(PromiseNeed.FIRST_ASSERTION, this));
+        combined.add(new Tuple<>(PromiseNeed.SECOND_ASSERTION, statement));
+
+        Result<Boolean> result = items ->
+                items.getBoolean(PromiseNeed.FIRST_ASSERTION)
+                        && items.getBoolean(PromiseNeed.SECOND_ASSERTION);
+
+        return slamTogether(result, combined);
+    }
+
+    public Statement or(Statement statement) {
+        List<Tuple<PromiseNeed, Statement>> combined = new ArrayList<>();
+        combined.add(new Tuple<>(PromiseNeed.FIRST_ASSERTION, this));
+        combined.add(new Tuple<>(PromiseNeed.SECOND_ASSERTION, statement));
+
+        Result<Boolean> result = items ->
+                items.getBoolean(PromiseNeed.FIRST_ASSERTION)
+                        || items.getBoolean(PromiseNeed.SECOND_ASSERTION);
+
+        return slamTogether(result, combined);
     }
 
     public void setReturnValue(boolean value) {
