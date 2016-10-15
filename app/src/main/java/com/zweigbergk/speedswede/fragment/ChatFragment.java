160cf761c114075aab14595222b26d55a1e177b3
Jonathan Kran <jonathan.kran1@gmail.com>
2016-10-15 23:37:44 +0200

    Add an arcing layout for showing chat actions

--- app/src/main/java/com/zweigbergk/speedswede/fragment/ChatFragment.java
+++ app/src/main/java/com/zweigbergk/speedswede/fragment/ChatFragment.java
@@ -1,7 +1,12 @@
 package com.zweigbergk.speedswede.fragment;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.content.Context;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.ActionBar;
@@ -14,13 +19,21 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AnticipateInterpolator;
+import android.view.animation.OvershootInterpolator;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.ogaclejapan.arclayout.ArcLayout;
 import com.zweigbergk.speedswede.R;
 import com.zweigbergk.speedswede.activity.ChatActivity;
 import com.zweigbergk.speedswede.core.Chat;
 import com.zweigbergk.speedswede.database.LocalStorage;
+import com.zweigbergk.speedswede.eyecandy.AnimatorUtils;
 import com.zweigbergk.speedswede.presenter.ChatFragmentPresenter;
+import com.zweigbergk.speedswede.util.collection.ArrayList;
+import com.zweigbergk.speedswede.util.collection.List;
 import com.zweigbergk.speedswede.util.methodwrapper.CallerMethod;
 import com.zweigbergk.speedswede.util.methodwrapper.Client;
 import com.zweigbergk.speedswede.util.methodwrapper.ProviderMethod;
@@ -33,6 +46,12 @@ public class ChatFragment extends Fragment implements ChatFragmentView, Client<S
 
     private RecyclerView chatRecyclerView;
     private EditText mInputBox;
+
+    private View menuLayout;
+    private ArcLayout arcLayout;
+    private FloatingActionButton fab;
+
+    Toast toast = null;
 
     //TODO presenter between interactor and fragment
     private ChatFragmentPresenter mPresenter;
@@ -93,6 +112,18 @@ public class ChatFragment extends Fragment implements ChatFragmentView, Client<S
         chatRecyclerView = (RecyclerView) view.findViewById(R.id.fragment_chat_recycler_view);
         mInputBox = (EditText) view.findViewById(R.id.fragment_chat_message_text);
 
+        //Eyecandy
+        menuLayout = view.findViewById(R.id.menu_layout);
+        arcLayout = (ArcLayout) view.findViewById(R.id.arc_layout);
+        fab = (FloatingActionButton) view.findViewById(R.id.fab);
+
+        fab.setOnClickListener(this::onFabClick);
+
+        for (int i = 0, size = arcLayout.getChildCount(); i < size; i++) {
+            arcLayout.getChildAt(i).setOnClickListener(v -> showToast((Button) v));
+            arcLayout.getChildAt(i).setBackgroundResource(R.drawable.com_facebook_close);
+        }
+
         return view;
     }
 
@@ -122,12 +153,23 @@ public class ChatFragment extends Fragment implements ChatFragmentView, Client<S
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater){
         super.onCreateOptionsMenu(menu,inflater);
-        inflater.inflate(R.menu.menu_chat,menu);
+        inflater.inflate(R.menu.new_menu_chat,menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
+            case R.id.show_actions:
+                if (menuLayout.getVisibility() == View.VISIBLE) {
+                    hideMenu();
+                } else {
+                    showMenu();
+                }
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+        /*switch (item.getItemId()) {
             case R.id.blockUser:
                 mPresenter.onBanClicked();
                 mPresenter.terminateChat();
@@ -144,7 +186,7 @@ public class ChatFragment extends Fragment implements ChatFragmentView, Client<S
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
-        }
+        }*/
     }
 
     private void openDialog(){
@@ -192,5 +234,108 @@ public class ChatFragment extends Fragment implements ChatFragmentView, Client<S
     public void supply(String s) {
         getActivity().setTitle(s);
         mPresenter.onChangeNameClicked(getActivity().getBaseContext(), s);
+    }
+
+    private void showToast(Button btn) {
+        if (toast != null) {
+            toast.cancel();
+        }
+
+        String text = "Clicked: " + btn.getText();
+        toast = Toast.makeText(getActivity(), text, Toast.LENGTH_SHORT);
+        toast.show();
+
+    }
+
+    private void onFabClick(View v) {
+        if (v.isSelected()) {
+            hideMenu();
+        } else {
+            showMenu();
+        }
+        v.setSelected(!v.isSelected());
+    }
+
+    @SuppressWarnings("NewApi")
+    private void showMenu() {
+        menuLayout.setVisibility(View.VISIBLE);
+
+        List<Animator> animList = new ArrayList<>();
+
+        for (int i = 0, len = arcLayout.getChildCount(); i < len; i++) {
+            animList.add(createShowItemAnimator(arcLayout.getChildAt(i)));
+        }
+
+        AnimatorSet animSet = new AnimatorSet();
+        animSet.setDuration(400);
+        animSet.setInterpolator(new OvershootInterpolator());
+        animSet.playTogether(animList);
+        animSet.start();
+    }
+
+    @SuppressWarnings("NewApi")
+    private void hideMenu() {
+
+        java.util.List<Animator> animList = new java.util.ArrayList<>();
+
+        for (int i = arcLayout.getChildCount() - 1; i >= 0; i--) {
+            animList.add(createHideItemAnimator(arcLayout.getChildAt(i)));
+        }
+
+        AnimatorSet animSet = new AnimatorSet();
+        animSet.setDuration(400);
+        animSet.setInterpolator(new AnticipateInterpolator());
+        animSet.playTogether(animList);
+        animSet.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                menuLayout.setVisibility(View.INVISIBLE);
+            }
+        });
+        animSet.start();
+
+    }
+
+    private Animator createShowItemAnimator(View item) {
+
+        float dx = fab.getX() - item.getX();
+        float dy = 0 - item.getY();
+
+        item.setRotation(0f);
+        item.setTranslationX(dx);
+        item.setTranslationY(dy);
+
+        Animator anim = ObjectAnimator.ofPropertyValuesHolder(
+                item,
+                AnimatorUtils.rotation(0f, 720f),
+                AnimatorUtils.translationX(dx, 0f),
+                AnimatorUtils.translationY(dy, 0f)
+        );
+
+        return anim;
+    }
+
+    private Animator createHideItemAnimator(final View item) {
+        float dx = fab.getX() - item.getX();
+        float dy = 0 - item.getY();
+
+        Animator anim = ObjectAnimator.ofPropertyValuesHolder(
+                item,
+                AnimatorUtils.rotation(720f, 0f),
+                AnimatorUtils.translationX(0f, dx),
+                AnimatorUtils.translationY(0f, dy)
+        );
+
+        anim.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                item.setTranslationX(0f);
+                item.setTranslationY(0f);
+            }
+        });
+
+        return anim;
     }
 }
