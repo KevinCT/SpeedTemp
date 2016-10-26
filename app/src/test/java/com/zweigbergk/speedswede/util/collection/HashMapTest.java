9f5d97b3e17022047d59018115474396b2b960b4
petrosdeb <petrosd@student.chalmers.se>
2016-10-26 17:46:24 +0200

    HashMap HashSet

--- app/src/test/java/com/zweigbergk/speedswede/util/collection/HashMapTest.java
+++ app/src/test/java/com/zweigbergk/speedswede/util/collection/HashMapTest.java
@@ -1,60 +1,144 @@
 package com.zweigbergk.speedswede.util.collection;
 
+import com.zweigbergk.speedswede.util.methodwrapper.Client;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.Map;
 
 import static org.junit.Assert.*;
 
-/**
- * Created by Petros on 2016-10-19.
- */
 public class HashMapTest {
-    @Test
-    public void map() throws Exception {
 
+    HashMap<String, Integer> test;
+
+    @Before
+    public void setUp() throws Exception {
+        test = new HashMap<>();
+
+        assertTrue(test.isEmpty());
+
+        assertEquals(0, test.size());
     }
 
-    @Test
-    public void transform() throws Exception {
-
+    @After
+    public void tearDown() throws Exception {
+        test = null;
     }
 
+
     @Test
-    public void values() throws Exception {
+    public void values() throws Exception { //FEL
+        test = new HashMap<>();
+        Set<Integer> testSet = new HashSet<>();
+        for(int i=65;i<=90;i++) {
+            testSet.add(i);
+            test.put(String.valueOf((char) i) , i);
+        }
+
+        java.util.Set<String> keySet = test.keySet();
+        java.util.Set<Integer> valueSet = new HashSet<Integer>();
+
+        for(String i: keySet) {
+            valueSet.add(test.get(i));
+        }
+
+
+        assertEquals(testSet, valueSet);
 
     }
 
     @Test
     public void keys() throws Exception {
+        test = new HashMap<>();
+        Set<String> testSet = new HashSet<>();
+        for(int i=65;i<=90;i++) {
+            testSet.add(String.valueOf((char) i));
+            test.put(String.valueOf((char) i) , i);
+        }
 
+        assertEquals(testSet, test.keySet());
     }
 
-    @Test
-    public void foreach() throws Exception {
-
-    }
 
     @Test
     public void invert() throws Exception {
+        test = new HashMap<>();
 
-    }
+        test.put("a" , 1);
+        test.put("b" , 2);
+        test.put("c" , 3);
+        test.put("d" , 4);
 
-    @Test
-    public void filter() throws Exception {
+        HashMap<Integer, String> testInvert = (HashMap<Integer, String>) test.invert();
 
-    }
+        assertEquals(testInvert.get(1), "a");
+        assertEquals(testInvert.get(2), "b");
+        assertEquals(testInvert.get(3), "c");
+        assertEquals(testInvert.get(4), "d");
 
-    @Test
-    public void reject() throws Exception {
-
-    }
-
-    @Test
-    public void nonNull() throws Exception {
 
     }
 
     @Test
     public void create() throws Exception {
+        test = new HashMap<>();
+
+        Collection<String> keys = new ArrayList<>();
+        Collection<Integer> values = new ArrayList<>();
+
+        HashMap<String, Integer> hashMapCreateTest = new HashMap<>();
+
+        test.put("a", 1);
+        test.put("b", 2);
+        test.put("c", 3);
+        test.put("d", 4);
+
+        keys.add("a");
+        keys.add("b");
+        keys.add("c");
+        keys.add("d");
+
+        values.add(1);
+        values.add(2);
+        values.add(3);
+        values.add(4);
+
+        hashMapCreateTest = (HashMap<String, Integer>) HashMap.create(keys, values);
+
+        assertEquals(hashMapCreateTest, test);
+
+    }
+
+    @Test
+    public void putList() throws Exception {
+        test = new HashMap<>();
+        HashMap<String, Integer> hashMapPutListTest = new HashMap<>();
+
+        List<String> keys = new ArrayList<String>();
+
+        List<Integer> values = new ArrayList<Integer>();
+
+        test.put("a", 1);
+        test.put("b", 2);
+        test.put("c", 3);
+        test.put("d", 4);
+
+        keys.add("a");
+        keys.add("b");
+        keys.add("c");
+        keys.add("d");
+
+        values.add(1);
+        values.add(2);
+        values.add(3);
+        values.add(4);
+
+        hashMapPutListTest = hashMapPutListTest.putList(keys, values);
+
+        assertEquals(hashMapPutListTest, test);
 
     }
 
--- app/src/test/java/com/zweigbergk/speedswede/util/collection/HashSetTest.java
+++ app/src/test/java/com/zweigbergk/speedswede/util/collection/HashSetTest.java
@@ -1,39 +1,115 @@
 package com.zweigbergk.speedswede.util.collection;
 
+import org.hamcrest.core.StringContains;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 
 public class HashSetTest {
+    HashSet<String> test;
+
+    @Before
+    public void setUp() throws Exception {
+        test = new HashSet<>();
+
+        assertTrue(test.isEmpty());
+
+        assertEquals(0, test.size());
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        test = null;
+    }
+
     @Test
     public void union() throws Exception {
+        HashSet<String> testUnion = new HashSet<>();
+        HashSet<String> testUnion2 = new HashSet<>();
+
+        testUnion2.add("a");
+        testUnion2.add("b");
+        testUnion2.add("c");
+        testUnion2.add("d");
+        testUnion2.add("e");
+        testUnion2.add("f");
+
+        test.add("a");
+        test.add("b");
+        test.add("c");
+
+        testUnion.add("d");
+        testUnion.add("e");
+        testUnion.add("f");
+
+        testUnion = (HashSet<String>) testUnion.union(test);
+
+        assertEquals(testUnion2, testUnion);
 
     }
 
     @Test
     public void intersect() throws Exception {
+        test =  new HashSet<>();
+        HashSet<String> testIntersect = new HashSet<>();
+        HashSet<String> testIntersect2 = new HashSet<>();
+
+        test.add("a");
+        test.add("b");
+        test.add("c");
+        test.add("e");
+        test.add("f");
+        test.add("g");
+        test.add("h");
+
+        testIntersect2.add("a");
+        testIntersect2.add("c");
+        testIntersect2.add("e");
+        testIntersect2.add("g");
+        testIntersect2.add("k");
+
+        testIntersect.add("c");
+        testIntersect.add("a");
+        testIntersect.add("g");
+        testIntersect.add("e");
+
+        test = (HashSet<String>) test.intersect(testIntersect2);
+
+        assertEquals(test, testIntersect);
 
     }
 
     @Test
     public void difference() throws Exception {
+        test =  new HashSet<>();
+        HashSet<String> testIntersect = new HashSet<>();
+        HashSet<String> testIntersect2 = new HashSet<>();
 
+        test.add("a");
+        test.add("b");
+        test.add("c");
+        test.add("e");
+        test.add("f");
+        test.add("g");
+        test.add("h");
+
+        testIntersect2.add("a");
+        testIntersect2.add("c");
+        testIntersect2.add("e");
+        testIntersect2.add("g");
+        testIntersect2.add("k");
+
+        testIntersect.add("b");
+        testIntersect.add("f");
+        testIntersect.add("h");
+
+        test = (HashSet<String>) test.difference(testIntersect2);
+
+        assertEquals(test, testIntersect);
     }
 
-    @Test
-    public void reject() throws Exception {
-
-    }
-
-    @Test
-    public void foreach() throws Exception {
-
-    }
-
-    @Test
-    public void filter() throws Exception {
-
-    }
 
 }
